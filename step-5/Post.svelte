cd52657d24a062e9648d91920723548006243367
Holben888 <holben888@gmail.com>
2019-10-22 10:51:44 -0400

    add upvote and downvote step

---
+++ step-5/Post.svelte
@@ -0,0 +1,85 @@
+<script>
+  export let title = ''
+  export let content = ''
+  export let author = ''
+  export let votes = 0 // the "initial" votes before you started upvoting or downvoting
+  export let id = ''
+
+  let currentVotes = votes
+
+  let upvoted = false
+  let downvoted = false
+
+  const updateVotes = newVotes => {
+    const updates = {
+      postId: id,
+      votes: newVotes,
+    }
+    let options = {
+      method: 'POST', // to specify that we want to post something to the database
+      body: JSON.stringify(updates), // put the content we want to add in the body. Turn it into a string in order to send!
+    }
+    fetch(
+      'https://hackgt6-reddit-backend.bholmesdev.workers.dev',
+      options
+    ).catch(err => console.error(err)) // log if something bad happens
+  }
+
+  const upvote = () => {
+    upvoted = !upvoted // toggle the upvote flag
+    downvoted = false // set downvote to false, since we're trying to upvote now
+    /* 
+      if the upvote flag is true, then we add a vote.
+      If the flag is false, then we unselected our upvote, and need to bring the votes back down!
+    */
+    const newVotes = upvoted ? votes + 1 : votes
+    updateVotes(newVotes) // send API call to update votes
+    currentVotes = newVotes // update the votes locally to instantly see the change
+  }
+  const downvote = () => {
+    downvoted = !downvoted
+    upvoted = false
+    const newVotes = downvoted ? votes - 1 : votes
+    updateVotes(newVotes)
+    currentVotes = newVotes
+  }
+</script>
+
+<style>
+  section {
+    display: flex;
+    align-items: center;
+  }
+  button {
+    width: 30px;
+    height: 30px;
+  }
+  .votes {
+    margin-top: 10px;
+    margin-bottom: 10px;
+    text-align: center;
+  }
+  .button-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-right: 20px;
+  }
+</style>
+
+<section>
+  <div class="button-container">
+    <button on:click={upvote}>
+      <img src="/icons/up-arrow.svg" alt="" />
+    </button>
+    <p class="votes">{currentVotes}</p>
+    <button on:click={downvote}>
+      <img src="/icons/down-arrow.svg" alt="" />
+    </button>
+  </div>
+  <div>
+    <h1>{title}</h1>
+    <p>{author}</p>
+    <p>{content}</p>
+  </div>
+</section>
---
+++ step-5/index.svelte
@@ -0,0 +1,75 @@
+<script>
+  import Post from './Post.svelte'
+  import { onMount } from 'svelte'
+
+  let posts = []
+
+  let newPostTitle = ''
+  let newPostContent = ''
+
+  const username = 'Hugh Jazz'
+
+  const fetchPosts = () => {
+    fetch('https://hackgt6-reddit-backend.bholmesdev.workers.dev')
+      .then(response => response.json())
+      .then(json => (posts = json))
+  }
+
+  onMount(
+    () => {
+      fetchPosts() // run fetchPosts the first time (setInterval will call this function 5 seconds later)
+      const refresh = setInterval(() => fetchPosts, 5000) // use setInterval to run the fetchPosts function every 5 seconds
+    },
+    () => clearInterval(refresh) // fancy Svelte syntax here. Allows us to close the interval once the page is closed. Not necessary, but encouraged.
+  )
+
+  const addPost = () => {
+    if (newPostTitle === '' || newPostContent === '') {
+      return // don't create the post if either of the textboxes are empty
+    }
+
+    const newPost = {
+      title: newPostTitle,
+      content: newPostContent,
+      author: username,
+      votes: 0,
+    }
+
+    let options = {
+      method: 'POST', // to specify that we want to post something to the database
+      body: JSON.stringify(newPost), // put the content we want to add in the body. Turn it into a string in order to send!
+    }
+    fetch(
+      'https://hackgt6-reddit-backend.bholmesdev.workers.dev',
+      options
+    ).catch(err => console.error(err)) // log if something bad happens
+
+    posts = [...posts, newPost] // add the post to the local array of "posts" so you can see the addition instantly
+    newPostTitle = ''
+    newPostContent = ''
+  }
+</script>
+
+<style>
+  form {
+    display: flex;
+    flex-direction: column;
+  }
+</style>
+
+<form on:submit|preventDefault={addPost}>
+  <label>Title</label>
+  <input bind:value={newPostTitle} />
+  <label>Woke content</label>
+  <textarea bind:value={newPostContent} />
+  <button type="submit">Post!</button>
+</form>
+
+{#each posts as post}
+  <Post
+    id={post.id}
+    title={post.title}
+    content={post.content}
+    author={post.author}
+    votes={post.votes} />
+{/each}
